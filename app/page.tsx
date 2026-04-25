a8854db980c61d008906660cd1dcb7f3192843ed
tcultivator <lpanahon06@gmail.com>
2026-04-26 04:44:02 +0800

    update the navbar, also added a dashboard with the example layout of products

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,13 @@
-"use client"
 
-import { signOut, useSession } from "next-auth/react"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
 import Navbar from "./components/navbar/NavBar"
+import Dashboard from "./components/dashboard/dashboard"
 export default function Home() {
-  const { data: session, status } = useSession()
-  const router = useRouter()
 
-  useEffect(() => {
-    if (status === "loading") return
-
-    if (!session) {
-      router.push("/login")
-    }
-  }, [session, status, router])
-
-  if (!session) {
-    return null // or loading screen
-  }
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-[100dvh] bg-gray-100">
       <Navbar />
-
-      <div className="flex items-center justify-center h-[calc(100vh-64px)]">
-        <div className="text-center">
-         
-        </div>
-      </div>
+      <Dashboard />
     </div>
   )
 }

247001f985c5eccf2badee5f85a37680966cb95d
tcultivator <lpanahon06@gmail.com>
2026-04-28 01:48:37 +0800

    added a uploder route to upload data in database

---
+++ app/api/contribute/route.ts
@@ -0,0 +1,54 @@
+import db from '@/lib/db';
+
+export async function POST(req: Request) {
+    try {
+        const body = await req.json();
+
+        const {
+            product_name,
+            product_image,
+            price,
+            category,
+            description,
+            lat,
+            long,
+            store_name
+        } = body.addedProducts;
+        const {
+            email
+        } = body.session.user;
+
+        const [result] = await db.execute(
+            `INSERT INTO products_info 
+      (product_name, product_image, description, price, contributor_email, product_lat, product_long,category,store_name)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            [
+                product_name,
+                product_image,
+                description == '' ? 'No description' : description,
+                price,
+                email,
+                lat,
+                long,
+                category,
+                store_name
+            ]
+        );
+
+        return Response.json({
+            success: true,
+            message: 'Product inserted successfully',
+            result,
+        });
+    } catch (error: any) {
+        console.error(error);
+
+        return Response.json(
+            {
+                success: false,
+                message: error.message || 'Something went wrong',
+            },
+            { status: 500 }
+        );
+    }
+}
